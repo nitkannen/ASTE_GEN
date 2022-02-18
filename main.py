a0f768fa8c16693cec3f56921982e88804e5d331
nitkannen <nithishkannen@gmail.com>
2022-02-19 00:25:32 +0530

    checkpoints updated

--- main.py
+++ main.py
@@ -150,7 +150,7 @@ class T5FineTuner(pl.LightningModule):
         ### result cache
 
         self.best_f1 =-999999.0
-        self.best_checkpoint = None
+        self.best_checkpoint = "best_checkpoint_dev"
         self.best_epoch = None
 
 
@@ -261,6 +261,7 @@ class T5FineTuner(pl.LightningModule):
         if f > self.best_f1:
             self.best_f1 = f
             self.best_epoch = self.current_epoch
+            torch.save(self.model.state_dict(), self.best_checkpoint)
 
 
         self.log('step', self.current_epoch)
@@ -413,6 +414,14 @@ def evaluate(data_loader, model):
     _, _, aspect_f = get_f1_for_trainer(decoded_preds, decoded_labels , 'aspect')
     _, _, sentiment_f = get_f1_for_trainer(decoded_preds, decoded_labels , 'sentiment')
 
+    custom_print('\nTest Results\n')
+    custom_print('Test Opinion F1:', round(opinion_f, 3))
+    custom_print('Test Aspect F1:', round(aspect_f, 3))
+    custom_print('Test Sentiment F1:', round(sentiment_f, 3))
+    custom_print('Test P:', round(p, 3))
+    custom_print('Test R:', round(r, 3))
+    custom_print('Test F1:', round(f, 3))
+
     return {'f1':f, 'prec': p, 'rec': r , 'opinion': opinion_f, 'aspect': aspect_f, 'sentiment': sentiment_f }
 
 if __name__ == '__main__':
@@ -474,7 +483,7 @@ if __name__ == '__main__':
     if args.do_eval:
 
         custom_print("\n****** Conduct Evaluating ******")
-
+        trainer.test(model)
         # model = T5FineTuner(args)
         dev_results, test_results = {}, {}
         best_f1, best_checkpoint, best_epoch = -999999.0, None, None
@@ -484,8 +493,7 @@ if __name__ == '__main__':
         saved_model_dir = os.path.join(saved_model_dir, 'lightning_logs/version_1/checkpoints/')
         for f in os.listdir(saved_model_dir):
             file_name = os.path.join(saved_model_dir, f)
-            if 'cktepoch' in file_name:
-                all_checkpoints.append(file_name)
+            all_checkpoints.append(file_name)
 
         print(f"We will perform validation on the following checkpoints: {all_checkpoints}")
 
@@ -493,53 +501,58 @@ if __name__ == '__main__':
             # load dev and test datasets
         dev_dataset = ASTE_Dataset(tokenizer, data_path=args.dev_dataset_path, task=args.task, max_len=args.max_seq_length)
         dev_loader = DataLoader(dev_dataset, batch_size=32)
-
+        
         test_dataset = ASTE_Dataset(tokenizer, data_path=args.test_dataset_path, task=args.task, max_len=args.max_seq_length)
         test_loader = DataLoader(test_dataset, batch_size=32)
-
         for checkpoint in all_checkpoints:
-            epoch = checkpoint.split('=')[-1][:-5] if len(checkpoint) > 1 else ""
-            # only perform evaluation at the specific epochs ("15-19")
-            # eval_begin, eval_end = args.eval_begin_end.split('-')
-            if 0 <= int(epoch) < 100:
-                all_epochs.append(epoch)
+            custom_print('****Loading Checkpoint***************: ', checkpoint)
+            model_ckpt = torch.load(checkpoint)
+            tuner = T5FineTuner(args)
+            tuner.model.load_state_dict(model_ckpt['state_dict'])
+            _ = evaluate(test_loader, tuner)
+    #     for checkpoint in all_checkpoints:
+    #         epoch = checkpoint.split('=')[-1][:-5] if len(checkpoint) > 1 else ""
+    #         # only perform evaluation at the specific epochs ("15-19")
+    #         # eval_begin, eval_end = args.eval_begin_end.split('-')
+    #         if 0 <= int(epoch) < 100:
+    #             all_epochs.append(epoch)
 
-                # reload the model and conduct inference
-                print(f"\nLoad the trained model from {checkpoint}...")
-                model_ckpt = torch.load(checkpoint)
-                tuner = T5FineTuner(model_ckpt['hyper_parameters'])
-                tuner.load_state_dict(model_ckpt['state_dict'])
-                model = tuner.model
-                model.to('cuda')
+    #             # reload the model and conduct inference
+    #             print(f"\nLoad the trained model from {checkpoint}...")
+    #             model_ckpt = torch.load(checkpoint)
+    #             tuner = T5FineTuner(model_ckpt['hyper_parameters'])
+    #             tuner.load_state_dict(model_ckpt['state_dict'])
+    #             model = tuner.model
+    #             model.to('cuda')
                 
-                dev_result = evaluate(dev_loader, model)
-                if dev_result['f1'] > best_f1:
-                    best_f1 = dev_result['f1']
-                    best_checkpoint = checkpoint
-                    best_epoch = epoch
+    #             dev_result = evaluate(dev_loader, model)
+    #             if dev_result['f1'] > best_f1:
+    #                 best_f1 = dev_result['f1']
+    #                 best_checkpoint = checkpoint
+    #                 best_epoch = epoch
 
-                # add the global step to the name of these metrics for recording
-                # 'f1' --> 'f1_1000'
-                dev_result = dict((k + '_{}'.format(epoch), v) for k, v in dev_result.items())
-                dev_results.update(dev_result)
+    #             # add the global step to the name of these metrics for recording
+    #             # 'f1' --> 'f1_1000'
+    #             dev_result = dict((k + '_{}'.format(epoch), v) for k, v in dev_result.items())
+    #             dev_results.update(dev_result)
 
-                test_result = evaluate(test_loader, model)
-                test_result = dict((k + '_{}'.format(epoch), v) for k, v in test_result.items())
-                test_results.update(test_result)
+    #             test_result = evaluate(test_loader, model)
+    #             test_result = dict((k + '_{}'.format(epoch), v) for k, v in test_result.items())
+    #             test_results.update(test_result)
 
-    # print test results over last few steps
-        custom_print(f"\n\nThe best checkpoint is {best_checkpoint}")
-        best_step_metric = f"f1_{best_epoch}"
-        custom_print(f"F1 scores on test set: {test_results[best_step_metric]:.4f}")
+    # # print test results over last few steps
+    #     custom_print(f"\n\nThe best checkpoint is {best_checkpoint}")
+    #     best_step_metric = f"f1_{best_epoch}"
+    #     custom_print(f"F1 scores on test set: {test_results[best_step_metric]:.4f}")
 
-        custom_print("\n* Results *:  Dev  /  Test  \n")
-        metric_names = ['f1', 'prec', 'rec']
-        for epoch in all_epochs:
-            custom_print(f"Epoch-{epoch}:")
-            for name in metric_names:
-                name_step = f'{name}_{epoch}'
-                print(f"{name:<10}: {dev_results[name_step]:.4f} / {test_results[name_step]:.4f}", sep='  ')
-            custom_print()
+    #     custom_print("\n* Results *:  Dev  /  Test  \n")
+    #     metric_names = ['f1', 'prec', 'rec']
+    #     for epoch in all_epochs:
+    #         custom_print(f"Epoch-{epoch}:")
+    #         for name in metric_names:
+    #             name_step = f'{name}_{epoch}'
+    #             print(f"{name:<10}: {dev_results[name_step]:.4f} / {test_results[name_step]:.4f}", sep='  ')
+    #         custom_print()
 
 
 
