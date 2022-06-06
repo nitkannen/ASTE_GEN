3134b94933b1f32d861635e605289a30ded9a61a
nitkannen <nithishkannen@gmail.com>
2022-06-06 21:22:00 +0530

    hyp script lap14

--- scripts/lap14/hyp.sh
+++ scripts/lap14/hyp.sh
@@ -1,3 +1,4 @@
+
 python main.py --task lap14 \
                 --train_dataset_path lap14/train \
                 --dev_dataset_path lap14/dev \
@@ -12,7 +13,8 @@ python main.py --task lap14 \
                 --num_train_epochs 20 \
                 --regressor True \
                 --use_tagger True \
-                --logger_name lap14_logs_hyp_search2_2.txt \
+                --beta 0.4
+                --logger_name lap14_logs_hyp_search2_2_0.4.txt \
                 --log_message hyp_search \
                 --gpu_id 1 
 
@@ -23,14 +25,15 @@ python main.py --task lap14 \
                 --model_name_or_path t5-base \
                 --do_train \
                 --do_eval \
-                --train_batch_size 4 \
+                --train_batch_size 2 \
                 --gradient_accumulation_steps 2 \
                 --eval_batch_size 16 \
                 --learning_rate 3e-4 \
                 --num_train_epochs 20 \
                 --regressor True \
                 --use_tagger True \
-                --logger_name lap14_logs_hyp_search4_2.txt \
+                --beta 0.1 \
+                --logger_name lap14_logs_hyp_search2_2_0.1.txt \
                 --log_message hyp_search \
                 --gpu_id 1 
 
@@ -42,49 +45,12 @@ python main.py --task lap14 \
                 --model_name_or_path t5-base \
                 --do_train \
                 --do_eval \
-                --train_batch_size 4 \
-                --gradient_accumulation_steps 4 \
+                --train_batch_size 2 \
+                --gradient_accumulation_steps 2 \
                 --eval_batch_size 16 \
                 --learning_rate 3e-4 \
                 --num_train_epochs 20 \
-                --regressor True \
                 --use_tagger True \
-                --logger_name lap14_logs_hyp_search_4_4.txt \
-                --log_message hyp_search \
-                --gpu_id 1 
-
-python main.py --task lap14 \
-                --train_dataset_path lap14/train \
-                --dev_dataset_path lap14/dev \
-                --test_dataset_path lap14/test \
-                --model_name_or_path t5-base \
-                --do_train \
-                --do_eval \
-                --train_batch_size 4 \
-                --gradient_accumulation_steps 4 \
-                --eval_batch_size 16 \
-                --learning_rate 1e-4 \
-                --num_train_epochs 20 \
-                --regressor True \
-                --use_tagger True \
-                --logger_name lap14_logs_hyp_search_4_4_1e.txt \
-                --log_message hyp_search \
-                --gpu_id 1 
-
-python main.py --task lap14 \
-                --train_dataset_path lap14/train \
-                --dev_dataset_path lap14/dev \
-                --test_dataset_path lap14/test \
-                --model_name_or_path t5-base \
-                --do_train \
-                --do_eval \
-                --train_batch_size 4 \
-                --gradient_accumulation_steps 4 \
-                --eval_batch_size 16 \
-                --learning_rate 5e-4 \
-                --num_train_epochs 20 \
-                --regressor True \
-                --use_tagger True \
-                --logger_name lap14_logs_hyp_search_4_4_5e.txt \
+                --logger_name lap14_logs_hyp_search2_2_noregressor.txt \
                 --log_message hyp_search \
                 --gpu_id 1 
